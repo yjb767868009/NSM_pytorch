40f176157ac493a70fb7bc864418b782f6923ddd
fish <767868009@qq.com>
2020-11-23 20:48:20 +0800

    update data preprocess

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import argparse
 
-from model.utils.data_preprocess import data_preprocess
-from model.utils.initialization import initialization
-
 import torch.utils
 import torch.utils.cpp_extension
+
+from model.utils.data_preprocess import data_preprocess
+from model.utils.initialization import initialization
 
 # Check GPU available
 if torch.cuda.is_available():
@@ -15,13 +15,15 @@ if torch.cuda.is_available():
 parser = argparse.ArgumentParser(description='Train')
 parser.add_argument('--cache', default=True, help='cache: if set as TRUE all the training data will be loaded at once'
                                                   ' before the training start. Default: TRUE')
-parser.add_argument('--data_preprocess', default=False, help='preprocess dataset input and output')
+parser.add_argument('--data_preprocess', default="", help='preprocess dataset input and output')
 parser.add_argument('--train_Base', default=True, help='train nsm generate model')
 parser.add_argument('--train_GAN', default=True, help='train Refiner GAN')
 parser.add_argument('--test', default=False, help='test model')
 opt = parser.parse_args()
 
 if __name__ == '__main__':
+    if opt.data_preprocess != "":
+        data_preprocess(opt.data_preprocess)
     if opt.train_Base:
         base_model = initialization("base_model", cache=True)
         base_model.train()
