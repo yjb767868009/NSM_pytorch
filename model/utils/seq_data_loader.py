8814119e1ef6e34674b45730bfc9b2df49fba68a
fish <767868009@qq.com>
2020-11-24 13:58:53 +0800

    update data load

--- model/utils/seq_data_loader.py
+++ model/utils/seq_data_loader.py
@@ -5,7 +5,8 @@ from .seq_data_set import DataSet
 
 def load_data(input_dir, label_dir, cache=True):
     data_source = DataSet(input_dir, label_dir, cache)
-    data_source.load_all_data()
+    if cache:
+        data_source.load_all_data()
     train_source = data_source
     test_source = data_source
     return train_source, test_source
